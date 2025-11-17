c647e9429bd76801ffbd9b83ab6633fa4b391016
JAY <JAY@oonne.com>
2025-11-17 11:08:27 +0800

    feat: 用户禁用

--- miniprogram/pages/exception/error/index.ts
+++ miniprogram/pages/exception/error/index.ts
@@ -9,12 +9,15 @@ Page({
   data: {
     title: '', // 页面标题
     content: '', // 说明文案
+    showReloadBtn: true, // 是否显示重新进入按钮
   },
 
   onLoad(e) {
+    console.log(e);
     this.setData({
       title: e.title || '出错啦',
       content: e.content || '',
+      showReloadBtn: e.showReloadBtn === 'false' ? false : true,
     });
   },
 
